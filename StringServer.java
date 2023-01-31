863f2725aa5a8193dd1515f4227c353467b7e6ea
KristopherManalo <kristophermmanalo@gmail.com>
2023-01-30 17:50:45 -0800

    Fixed implementation

--- StringServer.java
+++ StringServer.java
@@ -2,29 +2,26 @@ import java.io.IOException;
 import java.net.URI;
 
 class Handler implements URLHandler {
-    String dictionary = new String();
-    if(url.getPath().contains("/add-message") { 
-        String[] parameters = url.getQuery().split("=");
-        if(parameters[0].equals("s")) {
-            dictionary.append(parameters[1]+ "\n");    
+    String returnString = "";
+    public String handleRequest(URI url) {     
+        if(url.getPath().contains("/add-message")) {
+            String[] parameters = url.getQuery().split("=");
+            if(parameters[0].equals("s")) {
+                returnString = returnString + parameters[1] + "\n";
+                return String.format(returnString);
+            }
         }
-        return String.format(dictionary);
-    }
-    else {
-        return "404 Not Found!";
+    return "404 Not Found";
     }
 }
 
 class StringServer {
-        public static void main(String[] args) throws IO Exception {
+    public static void main(String[] args) throws IOException {
         if(args.length == 0) {
-            System.out.println("Missing port number! Try any number"
-                      " between 1024 and  49151");
-            return;
-        } 
-
+        System.out.println("Missing port number: Insert number 1024 to 49151");
+        return;
+        }
         int port = Integer.parseInt(args[0]);
         Server.start(port, new Handler());
-	}
+    }
 }
-
